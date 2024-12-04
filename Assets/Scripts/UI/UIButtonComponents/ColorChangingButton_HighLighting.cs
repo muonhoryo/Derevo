f9a7c7b4c41f0b06aec67c403cd20dcddbc731f9
muonhoryo <muon1327@mail.ru>
2024-12-04 10:13:08 +0500

    A0.1.1.0 (Add UIButton and ColorChanging component)

---
+++ Assets/Scripts/UI/UIButtonComponents/ColorChangingButton_HighLighting.cs
@@ -0,0 +1,30 @@
+
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Derevo.UI
+{
+    public sealed class ColorChangingButton_HighLighting : MonoBehaviour
+    {
+        [SerializeField] private Graphic TargetGraphic;
+        [SerializeField] private UIButton TargetButton;
+        [SerializeField] private Color NormalColor;
+        [SerializeField] private Color HighLightColor;
+
+        private void Awake()
+        {
+            TargetButton.OnPointerEnterEvent += OnPointerEnter;
+            TargetButton.OnPointerExitEvent += OnPointerExit;
+            OnPointerExit();
+        }
+        private void OnPointerExit()
+        {
+            TargetGraphic.color = NormalColor;
+        }
+        private void OnPointerEnter()
+        {
+            TargetGraphic.color = HighLightColor;
+        }
+    }
+}
